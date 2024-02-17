aa4ed4970fbeec413576f782a834545b50754a6d
Negrea-Andrei <negrea97@yahoo.com>
2024-02-17 19:38:46 +0200

    place the components

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+    return (
+        <div className="col mb-5">
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src="https://via.placeholder.com/200"
+              class="card-img-top"
+              alt="image"
+            />
+          </div>
+        </div>
+      );
+}
